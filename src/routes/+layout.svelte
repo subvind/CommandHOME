53731db42e039abc6360a566ee60f22572a8ae87
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-11-09 18:48:40 -0600

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  
+  import { HeaderDefault } from "subvind-components"
+  import SubHeader from "$lib/SubHeader.svelte"
+  import Footer from "$lib/Footer.svelte"
+
+  let organization: any;
+	let backendHostname: any = '';
+
+  onMount(async () => {
+    backendHostname = window.location.hostname
+    if (backendHostname === 'localhost') {
+      backendHostname = 'desknomy.subvind.com'
+    }
+    const response = await fetch(`https://api.subvind.com/organizations/backendHostname/${backendHostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      organization = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  })
+</script>
+
+{#if organization}
+  <HeaderDefault organization={organization} />
+{:else}
+  <nav class="grey darken-3">
+    <div class="nav-wrapper">
+    </div>
+  </nav>
+{/if}
+<SubHeader />
+
+<slot />
+
+<Footer />
+
+
+<style>
+  :global(body) {
+    background: #eee;
+  }
+</style>
